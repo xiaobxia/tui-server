c082839042e7fc3d37f94dd00f737fdf1a426246
xiaobxia <673806687@qq.com>
2019-07-08 13:33:09 +0800

    xg

--- app/schedules/inedx.js
+++ app/schedules/inedx.js
@@ -1,8 +1,8 @@
 // const env = process.env.NODE_ENV
 // const isDev = env === 'dev'
-exports.addDay = require('./addDay')
-exports.addNewDay = require('./addNewDay')
-exports.updateDayData = require('./updateDayData')
+// exports.addDay = require('./addDay')
+// exports.addNewDay = require('./addNewDay')
+// exports.updateDayData = require('./updateDayData')
 // if (!isDev) {
 //
 // }
